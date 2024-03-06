0f622d1f6f42670b797df62adde0d55e8e92302c
lunaytik <nathandeuil13@gmail.com>
2024-03-06 10:37:05 +0100

    feat: add exception filter for prisma to catch unique field duplication

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, PrismaModule],
+  imports: [ConfigModule.forRoot(), UsersModule],
   controllers: [],
   providers: [],
 })
