a464d1a9c6ab8b8c229cef8aa3dc775df3603cdf
lunaytik <nathandeuil13@gmail.com>
2024-03-06 11:01:47 +0100

    fix: update user change to use update dto

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User as UserModel } from '@prisma/client';
 import {CreateUserDto} from "./dto/create-user.dto";
+import {UpdateUserDto} from "./dto/update-user.dto";
 
 @Controller('users')
 export class UsersController {
@@ -21,17 +22,17 @@ export class UsersController {
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<UserModel> {
+  async findOne(@Param('id', ParseIntPipe) id: number): Promise<UserModel> {
     return this.usersService.user({ id: Number(id) });
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() userData: { email: string, password: string }): Promise<UserModel> {
-    return this.usersService.update({ where: { id: Number(id) }, data: userData});
+  async update(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto): Promise<UserModel> {
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string): Promise<UserModel> {
-    return this.usersService.remove({ id: Number(id) });
+  async remove(@Param('id', ParseIntPipe) id: number): Promise<UserModel> {
+    return this.usersService.remove({ id: id });
   }
 }
