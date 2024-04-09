63a18029e3a267c9a2b266f9e3af2b9977903727
julesbsz <j.bousrez@outlook.com>
2024-04-09 23:28:57 +0200

    feat: added shadows to 3d viewer

--- frontend/src/components/CanvasScene.jsx
+++ frontend/src/components/CanvasScene.jsx
@@ -1,11 +1,10 @@
-import { useGLTF, Environment, OrbitControls, ContactShadows } from "@react-three/drei";
+import { Environment, OrbitControls, ContactShadows } from "@react-three/drei";
 import React, { useEffect, useRef } from "react";
-import { useThree, useFrame, Canvas } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 function Glasses({ currentModelPath, ...props }) {
 	const groupRef = useRef();
-	const { scene } = useThree();
 
 	useEffect(() => {
 		if (currentModelPath) {
@@ -27,24 +26,22 @@ function Glasses({ currentModelPath, ...props }) {
 		};
 	}, [currentModelPath]);
 
-	useFrame(() => {
-		// Rotate the glasses
-	});
-
 	return <group ref={groupRef} {...props}></group>;
 }
 
 export default function App({ currentModelPath }) {
 	return (
-		<Canvas shadows camera={{ position: [0, 0, 4], fov: 50 }}>
+		<Canvas shadows camera={{ position: [0, 1.5, 4], fov: 50 }}>
 			<color attach="background" args={["#f0f0f0"]} />
 
 			<ambientLight intensity={10} />
 
-			<Glasses position={[-0.1, -0.2, 0]} rotation={[0, Math.PI / 2, 0]} scale={0.3} currentModelPath={currentModelPath} />
+			<Glasses position={[-0.2, 0.4, 1]} rotation={[0, -0.5, 0]} scale={1} currentModelPath={currentModelPath} />
 
 			<Environment preset="city" />
-			<OrbitControls minPolarAngle={Math.PI / 2.5} maxPolarAngle={Math.PI / 2.5} />
+			<OrbitControls makeDefault autoRotate autoRotateSpeed={0.1} minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
+
+			<ContactShadows rotation={[0, 0, 0]} position={[0, 0, 0]} opacity={1} width={1} height={1} blur={1.2} far={1} />
 		</Canvas>
 	);
 }
