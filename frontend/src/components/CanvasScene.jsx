7b05618880b97ea5033bd9652ba6acf92282a9f0
julesbsz <j.bousrez@outlook.com>
2024-04-09 11:11:47 +0200

    feat: v1 of 3d viewer

--- frontend/src/components/CanvasScene.jsx
+++ frontend/src/components/CanvasScene.jsx
@@ -1,36 +1,20 @@
-import { useRef } from "react";
-import { Canvas, useLoader } from "@react-three/fiber";
-import { useGLTF, Caustics, CubeCamera, Environment, OrbitControls, RandomizedLight, AccumulativeShadows, MeshRefractionMaterial, MeshTransmissionMaterial } from "@react-three/drei";
-import { EffectComposer, Bloom } from "@react-three/postprocessing";
-import { RGBELoader } from "three-stdlib";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Environment, OrbitControls, ContactShadows } from "@react-three/drei";
 
-function Diamond(props) {
-	const ref = useRef();
-	const glasses = useGLTF("/model_2.glb");
-
-	return <primitive object={glasses.scene} ref={ref} scale={[0.5, 0.5, 0.5]} castShadow receiveShadow {...props} />;
+function Glasses(props) {
+	const { scene } = useGLTF("/model_2.glb");
+	return <primitive object={scene} {...props} />;
 }
 
 export default function App() {
 	return (
-		<Canvas shadows shadowMap camera={{ position: [-5, 0.5, 5], fov: 45 }}>
-			<color attach="background" args={["#f0f0f0"]} />
+		<Canvas shadows camera={{ position: [0, 0, 4], fov: 50 }}>
+			<ambientLight intensity={0.5} />
 
-			<ambientLight intensity={4 * Math.PI} />
-			{/* <spotLight decay={0} position={[5, 5, -10]} angle={0.15} penumbra={1} castShadow /> */}
-			{/* <pointLight decay={0} position={[-10, -10, -10]} /> */}
+			<Glasses position={[-0.1, -0.2, 0]} rotation={[0, Math.PI / 2, 0]} scale={0.3} />
 
-			<Diamond rotation={[0, 0, 0]} position={[0, -0.175 + 0.5, 0]} />
-
-			<AccumulativeShadows temporal frames={100} color="orange" colorBlend={2} toneMapped={true} alphaTest={0.7} opacity={1} scale={12} position={[0, -0.5, 0]}>
-				<RandomizedLight amount={8} radius={10} ambient={0.5} position={[5, 5, -10]} bias={0.001} />
-			</AccumulativeShadows>
-
-			<OrbitControls makeDefault autoRotate autoRotateSpeed={0.1} minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
-
-			<EffectComposer>
-				<Bloom luminanceThreshold={1} intensity={2} levels={9} mipmapBlur />
-			</EffectComposer>
+			<Environment preset="city" />
+			<OrbitControls minPolarAngle={Math.PI / 2.5} maxPolarAngle={Math.PI / 2.5} />
 		</Canvas>
 	);
 }
