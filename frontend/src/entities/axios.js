7d76743f9d943e70665e0dcc05bdae8122970daa
lunaytik <nathandeuil13@gmail.com>
2024-04-10 19:14:25 +0200

    feat: change admin layout + change fetching url

--- frontend/src/entities/axios.js
+++ frontend/src/entities/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'http://mmi21h04.mmi-troyes.fr:3000/',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
@@ -13,7 +13,7 @@ api.interceptors.response.use((response) => {return response},
     (error) => {
         if (error?.response?.data?.statusCode === 401) {
             localStorage.removeItem('access_token');
-            location.href = '/admin/login';
+            location.href = '/admin';
         }
 
         return error;
--- frontend/src/pages/admin/AdminLayout.jsx
+++ frontend/src/pages/admin/AdminLayout.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Outlet } from "react-router-dom";
 
 import "../../styles/admin/admin.css";
+import Login from "./Login.jsx";
 
 function AdminLayout() {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        setCurrentUser(localStorage.getItem('access_token'));
+    }, []);
+
+    const logout = () => {
+        localStorage.removeItem('access_token');
+        location.href = '/admin';
+    }
+
   return (
     <div className="admin-grid">
       <div className="admin-grid-left">
@@ -12,7 +24,19 @@ function AdminLayout() {
 
       <div className="admin-grid-right">
         <div className="admin-outlet">
-          <Outlet />
+            {currentUser ? (
+                    <a onClick={logout} className="admin-back">Logout</a>
+                ) : (
+                    <></>
+                )
+            }
+
+            {currentUser ? (
+                    <Outlet />
+                ) : (
+                    <Login />
+                )
+            }
         </div>
       </div>
     </div>
